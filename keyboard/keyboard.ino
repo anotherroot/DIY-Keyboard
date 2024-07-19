50d373ee0f0bfbf7242e90d8d3c165fd3201e977
anotherroot <is.already.taken@pm.me>
2024-07-19 15:04:53 +0200

    redid how the keyboard is stored & reduced the space requirement & added basic support for holding of keys

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -1,6 +1,9 @@
 #include <Keyboard.h> 
 #include <Wire.h>
 
+#define PACK(kbd,row,column) (kbd*100+row*10+column)
+#define UNPACK(packed) Loc(packed/100,(packed%100)/10,packed%10)
+
 byte rows[] = { 8, 7, 6, 5, 4};
 const int rowCount = sizeof(rows)/sizeof(rows[0]);
 
@@ -43,7 +46,7 @@ void readMatrix() {
       byte old_state = matrix[rowIndex][colIndex];
       byte state = !digitalRead(rowCol);
       if(old_state^state){
-        processKeyEvent(0, rowIndex, colIndex, state);
+        processKeyEvent(PACK(0, rowIndex, colIndex), state);
       }
       matrix[rowIndex][colIndex] = state;
 			pinMode(rowCol, INPUT);
@@ -53,11 +56,6 @@ void readMatrix() {
 	}
 }
 
-void loop()
-{
-  readMatrix();
-  delay(10);
-}
 
 
 // function that executes whenever data is received from master
@@ -65,12 +63,13 @@ void receiveEvent(int howMany) {
   int row = Wire.read();
   int column = Wire.read();
   bool pressed = !Wire.read();
-  processKeyEvent(1,row,column,pressed);
+  processKeyEvent(PACK(1,row,column),pressed);
 }
 
 const byte Layer0 = 0;
 const byte Layer1 = 1;
 const byte Layer2 = 2;
+const byte Layer3 = 3;
 const byte CountLayer = 4;
 
 const byte ModeNone=0;
@@ -82,48 +81,55 @@ const byte ModeLayer=5;
 const byte ModeLayerMod=6;
 
 struct Layer {
-  char* name;
   byte layer;
   Layer(){
   }
-  Layer(char name[6], int layer):name(name),layer(layer){
+  Layer( int layer):layer(layer){
   }
 };
 
 struct Mod {
-  char* name;
-  byte mod;
+  byte keycode;
   Mod(){
   }
-  Mod(char name[6],short mod):name(name),mod(mod){
+  Mod(short keycode):keycode(keycode){
   }
 };
 
 struct Key{
   byte mode;
-  char* name;
   byte keycode;
   Mod mod;
   Layer layer;
-  Key():name("None"),mode(ModeNone){
+  Key():mode(ModeNone){
   }
-  Key(char name[6], byte keyCode)
-    :name(name),keycode(keycode),mode(ModeKey){
+  Key( byte keycode)
+    :keycode(keycode),mode(ModeKey){
   }
-  Key(char name[6], byte keyCode, Mod mod)
-    :name(name),keycode(keycode),mod(mod),mode(ModeKeyMod){
+  Key( byte keycode, Mod mod)
+    :keycode(keycode),mod(mod),mode(ModeKeyMod){
   }
-  Key(char name[6], Mod mod)
-    :name(name),mod(mod),mode(ModeMod){
+  Key( Mod mod)
+    :mod(mod),mode(ModeMod){
   }
-  Key(char name[6], byte keyCode, Layer layer)
-    :name(name),keycode(keycode),layer(layer),mode(ModeKeyLayer){
+  Key( byte keycode, Layer layer)
+    :keycode(keycode),layer(layer),mode(ModeKeyLayer){
   }
-  Key(char name[6], Layer layer)
-    :name(name),layer(layer),mode(ModeLayer){
+  Key( Layer layer)
+    :layer(layer),mode(ModeLayer){
   }
-  Key(char name[6], Layer layer, Mod mod)
-    :name(name),layer(layer),mode(ModeLayerMod){
+  Key( Layer layer, Mod mod)
+    :layer(layer),mode(ModeLayerMod){
+  }
+};
+struct Loc{
+  byte kbd;
+  byte row;
+  byte column;
+  Loc(){
+  }
+  Loc(byte kbd, byte row, byte column):kbd(kbd),row(row),column(column){
+  
   }
 };
 
@@ -131,76 +137,90 @@ typedef Key K;
 typedef Mod M;
 typedef Layer L;
 
-#define K_CTRL "Ctrl",KEY_LEFT_CTRL
-#define K_SHIFT "Shift",KEY_LEFT_SHIFT
-#define K_ALT "Alt",KEY_LEFT_ALT
-#define K_WIN "Win",KEY_LEFT_GUI
 
-#define M_CTRL Mod("Ctrl",KEY_LEFT_CTRL)
-#define M_SHIFT Mod("Shift",KEY_LEFT_SHIFT)
-#define M_ALT Mod("Alt",KEY_LEFT_ALT)
-#define M_WIN Mod("Win",KEY_LEFT_GUI)
 
-#define K_q "q",'q'
-#define K_w "w",'w'
-#define K_e "e",'e'
-#define K_r "r",'r'
-#define K_t "t",'t'
-#define K_y "y",'y'
-#define K_u "u",'u'
-#define K_i "i",'i'
-#define K_o "o",'o'
-#define K_p "p",'p'
-#define K_a "a",'a'
-#define K_s "s",'s'
-#define K_d "d",'d'
-#define K_f "f",'f'
-#define K_g "g",'g'
-#define K_h "h",'h'
-#define K_j "j",'j'
-#define K_k "k",'k'
-#define K_l "l",'l'
-#define K_z "z",'z'
-#define K_x "x",'x'
-#define K_c "c",'c'
-#define K_v "v",'v'
-#define K_b "b",'b'
-#define K_n "n",'n'
-#define K_m "m",'m'
+void printKeycode(byte keycode){
+  switch(keycode){
+    case KEY_LEFT_CTRL: { Serial.print("Ctrl"); } break;
+    case KEY_LEFT_SHIFT: { Serial.print("Shift"); } break;
+    case KEY_LEFT_ALT: { Serial.print("Alt"); } break;
+    case KEY_LEFT_GUI: { Serial.print("Win"); } break;
+    case KEY_RIGHT_CTRL: { Serial.print("Ctrl"); } break;
+    case KEY_RIGHT_SHIFT: { Serial.print("Shift"); } break;
+    case KEY_RIGHT_ALT: { Serial.print("Alt"); } break;
+    case KEY_RIGHT_GUI: { Serial.print("Win"); } break;
+    case 'q': { Serial.print("q"); } break;
+    case 'w': { Serial.print("w"); } break;
+    case 'e': { Serial.print("e"); } break;
+    case 'r': { Serial.print("r"); } break;
+    case 't': { Serial.print("t"); } break;
+    case 'y': { Serial.print("y"); } break;
+    case 'u': { Serial.print("u"); } break;
+    case 'i': { Serial.print("i"); } break;
+    case 'o': { Serial.print("o"); } break;
+    case 'p': { Serial.print("p"); } break;
+    case 'a': { Serial.print("a"); } break;
+    case 's': { Serial.print("s"); } break;
+    case 'd': { Serial.print("d"); } break;
+    case 'f': { Serial.print("f"); } break;
+    case 'g': { Serial.print("g"); } break;
+    case 'h': { Serial.print("h"); } break;
+    case 'j': { Serial.print("j"); } break;
+    case 'k': { Serial.print("k"); } break;
+    case 'l': { Serial.print("l"); } break;
+    case 'z': { Serial.print("z"); } break;
+    case 'x': { Serial.print("x"); } break;
+    case 'c': { Serial.print("c"); } break;
+    case 'v': { Serial.print("v"); } break;
+    case 'b': { Serial.print("b"); } break;
+    case 'n': { Serial.print("n"); } break;
+    case 'm': { Serial.print("m"); } break;
+    case ' ': { Serial.print("Space"); } break;
+    case KEY_RETURN: { Serial.print("Enter"); } break;
+    case KEY_TAB: { Serial.print("Tab"); } break;
+    case KEY_ESC: { Serial.print("Esc"); } break;
+    default: { Serial.print("None"); } break;
+  }
+}
 
-#define K_space "Space",' '
-#define K_enter "Enter",KEY_RETURN
-#define K_tab "Tab",KEY_TAB
-#define K_esc "Esc",KEY_ESC
+void printLayer(byte layer){
+  switch(layer){
+    case Layer0: { Serial.print("Layer0"); } break;
+    case Layer1: { Serial.print("Layer1"); } break;
+    case Layer2:{ Serial.print("Layer2"); } break;
+    case Layer3: { Serial.print("Layer3"); } break;
+    default: { Serial.print("Unknown Layer"); } break;
+  }
+}
 
 Key keyboard[CountLayer][2][rowCount][columnCount] = {
   {
     {
-      {K(K_t),       K(K_r),      K(K_tab),   K(K_esc,M_CTRL),    K(K_i)},
-      {K("L2", Layer2),       K(K_g),      K(K_e),     K(K_w),      K(K_q)},
-      {K(K_space),   K(K_f),      K(K_d),     K(K_s),      K(K_a)},
-      {K(K_CTRL),    K(K_i),      K(K_i),     K(K_i),      K(K_WIN)},
-      {K(K_ALT),     K(K_SHIFT),  K(K_i),     K(K_i),      K(K_i)},
+      {K('t'),       K('r'),      K(KEY_TAB),   K(KEY_ESC,M(KEY_RIGHT_CTRL)),    K('i')},
+      {K(Layer2),       K('g'),      K('e'),     K('w'),      K('q')},
+      {K(' '),   K('f'),      K('d'),     K('s'),      K('a')},
+      {K(KEY_LEFT_CTRL),    K('i'),      K('i'),     K('i'),      K(KEY_RIGHT_GUI)},
+      {K(KEY_LEFT_ALT),     K(KEY_LEFT_SHIFT),  K('i'),     K('i'),      K('i')},
     },
     {
-      {K(K_t),     K(K_r),      K(K_tab),   K(K_esc),    K(K_i)},
-      {K("L1", Layer1),     K(K_g),      K(K_e),     K(K_w),      K(K_q)},
-      {K(K_enter), K(K_f),      K(K_d),     K(K_s),      K(K_a)},
-      {K(K_CTRL),  K(K_i),      K(K_i),     K(K_i),      K(K_WIN)},
-      {K(K_ALT),   K(K_SHIFT),  K(K_i),     K(K_i),      K(K_i)},
+      {K('y'),     K('u'),      K(KEY_TAB),   K(KEY_ESC),    K('p')},
+      {K(Layer1),     K('h'),      K('i'),     K('o'),      K(';')},
+      {K(KEY_RETURN), K('j'),      K('k'),     K('l'),      K('a')},
+      {K(KEY_RIGHT_CTRL),  K(),      K(),     K(),      K(KEY_RIGHT_GUI)},
+      {K(KEY_RIGHT_ALT),   K(KEY_RIGHT_SHIFT),  K(),     K(),      K()},
     },
   },
   {
     {
       {K(),       K(),      K(),   K(),    K()},
-      {K("L0", Layer0),       K(),      K(),     K(),      K()},
+      {K(Layer0),       K(),      K(),     K(),      K()},
       {K(),   K(),      K(),     K(),      K()},
       {K(),    K(),      K(),     K(),      K()},
       {K(),     K(),  K(),     K(),      K()},
     },
     {
       {K(),     K(),      K(),   K(),    K()},
-      {K("L2", Layer2),     K(),      K(),     K(),      K()},
+      {K( Layer2),     K(),      K(),     K(),      K()},
       {K(), K(),      K(),     K(),      K()},
       {K(),  K(),      K(),     K(),      K()},
       {K(),   K(),  K(),     K(),      K()},
@@ -209,14 +229,14 @@ Key keyboard[CountLayer][2][rowCount][columnCount] = {
   {
     {
       {K(),       K(),      K(),   K(),    K()},
-      {K("L1", Layer1),       K(),      K(),     K(),      K()},
+      {K(Layer1),       K(),      K(),     K(),      K()},
       {K(),   K(),      K(),     K(),      K()},
       {K(),    K(),      K(),     K(),      K()},
       {K(),     K(),  K(),     K(),      K()},
     },
     {
       {K(),     K(),      K(),   K(),    K()},
-      {K("L0", Layer0),     K(),      K(),     K(),      K()},
+      {K(Layer0),     K(),      K(),     K(),      K()},
       {K(), K(),      K(),     K(),      K()},
       {K(),  K(),      K(),     K(),      K()},
       {K(),   K(),  K(),     K(),      K()},
@@ -226,14 +246,78 @@ Key keyboard[CountLayer][2][rowCount][columnCount] = {
 
 int layer = Layer0;
 
+struct PressedKey{
+  byte timePressed; // in x*10ms
+  byte data;
+#define DATA_LAYER_SPACE 3
+#define DATA_PRESSED_SPACE 4
+#define DATA_HOLD_SPACE 5
 
-void processKeyEvent(int kbd,int row, int column,bool pressed){
-  Serial.print("kbd:");
-  Serial.print(kbd);
-  Serial.print(", row:");
-  Serial.print(row);
-  Serial.print(", column:");
-  Serial.print(column);
+  PressedKey():data(0),timePressed(0){
+
+  }
+
+  byte getLayer(){
+    return data & ((2<<DATA_LAYER_SPACE) - 1);
+  }
+  bool isPressed(){
+    return data & 1<<DATA_PRESSED_SPACE;
+  }
+  bool hasHold(){
+    return data & 1<<DATA_HOLD_SPACE;
+  }
+  void setData(byte layer,bool has_hold,bool pressed){
+    data = layer & ((2<<DATA_LAYER_SPACE) - 1);
+    data += pressed ? 1<<DATA_PRESSED_SPACE: 0;
+    data += has_hold ? 1<<DATA_HOLD_SPACE: 0;
+  }
+
+  void press(byte layer, bool has_hold){
+    setData(layer,has_hold,true);
+  }
+  byte release(){
+    byte layer = getLayer();
+    setData(0,0,0);
+    timePressed = 0;
+    return layer;
+  }
+  void addTime(){
+    byte ismax = 255==timePressed;
+    timePressed=(1-ismax)*(timePressed+1) + ismax*(255);
+  }
+};
+
+typedef PressedKey PK;
+
+PressedKey pressedKeys[2][rowCount][columnCount] = {
+    {
+      {PK(),       PK(),      PK(),   PK(),    PK()},
+      {PK(),       PK(),      PK(),     PK(),      PK()},
+      {PK(),   PK(),      PK(),     PK(),      PK()},
+      {PK(),    PK(),      PK(),     PK(),      PK()},
+      {PK(),     PK(),  PK(),     PK(),      PK()},
+    },
+    {
+      {PK(),     PK(),      PK(),   PK(),    PK()},
+      {PK(),     PK(),      PK(),     PK(),      PK()},
+      {PK(), PK(),      PK(),     PK(),      PK()},
+      {PK(),  PK(),      PK(),     PK(),      PK()},
+      {PK(),   PK(),  PK(),     PK(),      PK()},
+    },
+};
+
+
+
+void processKeyEvent(byte packedLoc,bool pressed){
+  Loc loc = UNPACK(packedLoc);
+  Serial.print("packed:");
+  Serial.print(packedLoc);
+  Serial.print(", Loc.kbd:");
+  Serial.print(loc.kbd);
+  Serial.print(", Loc.row:");
+  Serial.print(loc.row);
+  Serial.print(", Loc.column:");
+  Serial.print(loc.column);
   if(pressed){
     Serial.print(", p -> ");
   }
@@ -241,37 +325,74 @@ void processKeyEvent(int kbd,int row, int column,bool pressed){
     Serial.print(", r -> ");
   }
 
-  Key *key = &keyboard[layer][kbd][row][column];
+
+  Key *key = &keyboard[0][loc.kbd][loc.row][loc.column];
+  
+  Serial.print(key->mode);
+  Serial.print(": ");
+  Serial.print(pressedKeys[loc.kbd][loc.row][loc.column].timePressed);
+  Serial.print(": ");
 
   if(key->mode == ModeNone){
-    Serial.print(key->name);
+    Serial.print("None");
   }
   else if(key->mode == ModeKey){
-    Serial.print(key->name);
+    printKeycode(key->keycode);
   }
   else if(key->mode == ModeKeyMod){
-    Serial.print(key->name);
+    printKeycode(key->keycode);
     Serial.print(", hold: ");
-    Serial.print(key->mod.name);
+    printKeycode(key->mod.keycode);
   }
   else if(key->mode == ModeMod){
-    Serial.print(key->mod.name);
+    printKeycode(key->mod.keycode);
   }
   else if(key->mode == ModeKeyLayer){
-    Serial.print(key->name);
+    printKeycode(key->keycode);
     Serial.print(", hold: ");
-    Serial.print(key->layer.name);
+    printLayer(key->layer.layer);
   }
   else if(key->mode == ModeLayer){
-    Serial.print(key->layer.name);
+    printLayer(key->layer.layer);
   }
   else if(key->mode == ModeLayerMod){
-    Serial.print(key->layer.name);
+    printLayer(key->layer.layer);
     Serial.print(", hold: ");
-    Serial.print(key->mod.name);
+    printKeycode(key->mod.keycode);
   }
 
+  bool has_hold = key->mode == ModeLayerMod || key->mode == ModeKeyLayer || key->mode == ModeKeyMod;
+  Serial.print(" ");
+  Serial.print(has_hold);
+
   Serial.println();          // new line for readability
+
+
+  PressedKey* keyp = &pressedKeys[loc.kbd][loc.row][loc.column];
+
+  if(pressed){
+    keyp->press(0,has_hold);
+    // Keyboard.press(keyboard[kbd][row][column]);
+  }
+  else{
+    keyp->release();
+    // Keyboard.release(keyboard[kbd][row][column]);
+  }
+
+  if(key->mode == ModeNone){
+  }
+  else if(key->mode == ModeKey){
+  }
+  else if(key->mode == ModeKeyMod){
+  }
+  else if(key->mode == ModeMod){
+  }
+  else if(key->mode == ModeKeyLayer){
+  }
+  else if(key->mode == ModeLayer){
+  }
+  else if(key->mode == ModeLayerMod){
+  }
 
   // if(pressed){
   //   Keyboard.press(keyboard[kbd][row][column]);
@@ -280,4 +401,36 @@ void processKeyEvent(int kbd,int row, int column,bool pressed){
   //   Keyboard.release(keyboard[kbd][row][column]);
   // }
 
+}
+
+#define DELAY 10
+#define HOLD_TIME 30
+
+void updatePressed(){
+  for(int i=0;i<2;++i){
+    for(int j=0;j<rowCount;++j){
+      for(int k=0;k<columnCount;++k){
+        PressedKey *pkey = &pressedKeys[i][j][k];
+        if(pkey->isPressed()){
+          pkey->addTime();
+
+          if(pkey->timePressed == HOLD_TIME){
+            Serial.print("Holding: ");
+            Serial.println(PACK(i,j,k));
+            if(pkey->hasHold()){
+              Serial.println("Activate hold");
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
+
+void loop()
+{
+  readMatrix();
+  delay(DELAY);
+  updatePressed();
 }
